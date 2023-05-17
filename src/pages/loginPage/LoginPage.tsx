ab6e631eddfa78e77fd613334a011c9bdaaf523d
Dmitry Bychinsky <dbychinsky@qulix.com>
2023-05-17 15:50:20 +0300

    build

--- src/pages/loginPage/LoginPage.tsx
+++ src/pages/loginPage/LoginPage.tsx
@@ -27,6 +27,7 @@ const LoginPage = observer(() => {
     useEffect(() => {
         authStore.clearFields();
         authStore.clearSessionStorage();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
     /**
