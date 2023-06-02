384bfc2b3f5683c762e5e4a2dd7c5b9fa42ce828
Dmitry Bychinsky <dbychinsky@qulix.com>
2023-06-02 08:47:48 +0300

    deploy build 2.0

--- src/pages/contactListPage/ContactListPage.tsx
+++ src/pages/contactListPage/ContactListPage.tsx
@@ -2,7 +2,6 @@ import React, {useContext, useEffect} from 'react';
 import ContactList from "../../component/contactList/ContactList";
 import {observer} from "mobx-react";
 import "./ContactListPage.scss";
-import Header from "../../component/header/Header";
 import ContactListReminderWidget from "../../component/contactListReminderWidget/ContactListReminderWidget";
 import H1 from "../../component/header/H1";
 import {RouterPathList} from "../../router/RouterPathList";
