71d9817a59e7bee3954a08da7fc98241c1cbb4d8
Bychinsky Dmitry <dbychinsky@qulix.com>
2024-04-11 15:37:25 +0300

    очищен репозиторий

--- src/utils/copyToClipboard.ts
+++
@@ -1,7 +0,0 @@
-/**
- * @description Копирование в буфер обмена.
- * @param {string} value - Value.
- */
-export async function copyToClipboard(value: string) {
-    navigator.clipboard.writeText(value).then();
-}
